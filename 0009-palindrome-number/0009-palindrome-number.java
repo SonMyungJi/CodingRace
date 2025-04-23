c4fca9491acb58c0bc3c132002a82a1b76dac2d9
SonMyungJi <133613716+SonMyungJi@users.noreply.github.com>
2025-04-23 22:15:01 +0900

    Time: 6 ms (30.88%), Space: 44.4 MB (26.46%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        String nums = Integer.toString(x);
+        for (int i=0; i<nums.length()/2; i++) {
+            if (nums.charAt(i) != nums.charAt(nums.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
